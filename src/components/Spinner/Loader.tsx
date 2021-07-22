59fafc234bcf9e22d0f64dd1b524d9e6b62355ba
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-22 21:57:23 +0200

    Create loader component

---
+++ src/components/Spinner/Loader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Loader.css';
+
+function Loader() {
+  return <div className="loader time-5 w-full h-full flex justify-center items-center"></div>;
+}
+
+export default Loader;
