cf004332ea5e9116dd80c17aa4867afc35da3741
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-22 14:45:32 +0200

    Put date in header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Header(): JSX.Element {
+  const date = useState(new Date().toLocaleDateString());
+
   return (
-    <div className="bg-gray-100 border border-b-black shadow-themeShadow hidden md:flex md:h-1/6">
-      <h2>Header</h2>
+    <div className="bg-gray-100 border border-b-black shadow-themeShadow hidden md:flex md:h-1/6 md:justify-end">
+      <p className="md:w-1/4 flex items-center justify-end text-2xl text-gray-500 p-10">{date}</p>
     </div>
   );
 }
