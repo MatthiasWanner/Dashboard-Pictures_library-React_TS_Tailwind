1ba92f29d6e881d930c28dd655676e43ccbf55dd
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-22 10:46:20 +0200

    Create personal shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        themeShadow: '5px 5px 10px 1px rgba(0, 0, 0, 0.3)',
+      },
+    },
   },
   variants: {
     extend: {},
