710e33e621d9b024065264e4d744e8b578c101c3
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-22 09:31:49 +0200

    Init commit. Setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+import Layout from './components/layout';
+
+function App() {
+  return (
+    <div className="App w-screen md:h-screen">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
