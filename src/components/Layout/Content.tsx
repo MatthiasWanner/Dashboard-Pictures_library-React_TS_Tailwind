975d4d107f08b549c647ae8c3faf3bb491223eb0
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-22 21:58:10 +0200

    Add className prop in content component

--- src/components/Layout/Content.tsx
+++ src/components/Layout/Content.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Content({ children }: { children: React.ReactNode }): JSX.Element {
-  return <div className="content bg-gray-100 w-full md:h-5/6 p-5">{children}</div>;
+function Content({ className, children }: { className?: string; children: React.ReactNode }): JSX.Element {
+  return <div className={`content bg-gray-100 w-full md:h-5/6 p-5 ${className}`}>{children}</div>;
 }
 
 export default Content;
