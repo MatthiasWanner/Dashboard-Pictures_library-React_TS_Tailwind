c577b5c342f1ecaccd5cc9da9df781b09091fe16
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-23 12:23:28 +0200

    Add handler in button component

--- src/components/FormComponents/ValidateButton.tsx
+++ src/components/FormComponents/ValidateButton.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 
 interface IProps {
   type: 'button' | 'submit' | 'reset' | undefined;
   className?: string;
   children: React.ReactNode;
+  handleClick?: () => void;
 }
 
-function ValidateButton({ children, className, type }: IProps): JSX.Element {
+function ValidateButton({ children, className, type, handleClick }: IProps): JSX.Element {
   return (
-    <button type={type} className={className}>
+    <button type={type} className={className ? className : ''} onClick={handleClick ? () => handleClick() : undefined}>
       {children}
     </button>
   );
