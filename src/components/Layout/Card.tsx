b51b9985f5c8b04aa1d8ff5e88b755d9dbd9ec7d
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-23 08:55:58 +0200

    Add padding @ card component

--- src/components/Layout/Card.tsx
+++ src/components/Layout/Card.tsx
@@ -8,9 +8,9 @@ interface IProps {
 
 function Card({ title, className, children }: IProps): JSX.Element {
   return (
-    <div className={`rounded-xl bg-customPurple text-white shadow-themeShadow ${className}`}>
+    <div className={`rounded-xl bg-customPurple text-white shadow-themeShadow p-5 ${className ? className : ''}`}>
       <div className="card-title font-medium text-3xl">{title}</div>
-      <div className={'card-content'}>{children}</div>
+      <div className={'card-content w-full h-full'}>{children}</div>
     </div>
   );
 }
--- src/components/Layout/Content.tsx
+++ src/components/Layout/Content.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Content({ className, children }: { className?: string; children: React.ReactNode }): JSX.Element {
-  return <div className={`content bg-gray-100 w-full md:h-5/6 p-5 ${className}`}>{children}</div>;
+  return <div className={`content bg-gray-100 w-full md:h-5/6 p-5 ${className ? className : ''}`}>{children}</div>;
 }
 
 export default Content;
